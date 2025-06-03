5603d9a2cafabdda71f9be44bcb524387964c82e
Itranar <72761418+Itranar@users.noreply.github.com>
2025-06-03 18:11:02 +0200

    Update zaglodzeniCzytenlnicy.c
    added else if

--- zaglodzeniCzytenlnicy.c
+++ zaglodzeniCzytenlnicy.c
@@ -68,10 +68,10 @@ void* reader_func(void* arg){
         //finished reading
         mtx->reader_count--;
         
-        if(mtx->reader_count <= 0){
+        if(mtx->reader_count <= 0 ){
             //printf("reader,writer\n");
             pthread_cond_signal(&mtx->writers_cond);
-        }else{
+        }else if( ! mtx->writer_queued ){
             //printf("reader,reader\n");
             pthread_cond_broadcast(&mtx->readers_cond);
         }
